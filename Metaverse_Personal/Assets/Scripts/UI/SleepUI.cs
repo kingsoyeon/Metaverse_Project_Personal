8ad1c97f5346559a314e8346cf785523225d77a9
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-20 02:32:38 +0900

    Fix: UI 초기화 방지
    bool값 isNo를 이용해 no 버튼을 눌러도 침대 근처에 있으면 ui 계속해서 뜨는 문제를 해결

--- Metaverse_Personal/Assets/Scripts/UI/SleepUI.cs
+++ Metaverse_Personal/Assets/Scripts/UI/SleepUI.cs
@@ -15,33 +15,55 @@ public class SleepUI : BaseUI
     // 침대 말풍선 UI
     public GameObject sleepUI;
 
+    // YES,NO 버튼
     public Button YesButton;
     public Button NoButton;
 
+    // SleepUI에서 NO를 눌렀는지
+    private bool isNo = false; // 처음은 안 누른 상태
+
     private void Start()
     {
-        
-
+       
         sleepUI.SetActive(false);
         YesButton.onClick.AddListener(OnYesButton);
         NoButton.onClick.AddListener(OnNoButton);
     }
     
-    
-
+   
     private void OnTriggerStay2D(Collider2D collision)
     {
         
-        if (collision.CompareTag("Player")) { sleepUI.SetActive(true); }
+        if (collision.CompareTag("Player")) 
+            // No를 누르지 않은 상태에서만 Ui활성화
+            {if (!isNo)
+            {
+                sleepUI.SetActive(true);
+            }
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) { sleepUI.SetActive(false); }
+        if (collision.CompareTag("Player")) 
+        { 
+            sleepUI.SetActive(false);
+            // 초기화
+            isNo = false;
+        }
     }
 
+    // yes 버튼 -> 미니게임씬으로 전환
     public void OnYesButton() { SceneManager.LoadScene("MiniGameScene"); }
-    public void OnNoButton() { sleepUI.SetActive(false); }
+
+    // NO 버튼 -> UI 비활성화 (no 버튼을 누르기 전까지)
+    public void OnNoButton() 
+    {  
+        sleepUI.SetActive(false);
+        // no 버튼을 눌렀으므로
+        isNo = true;
+        
+    }
 
    
 }
