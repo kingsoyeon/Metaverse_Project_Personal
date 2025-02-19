d88e0623bbeb5854496104f4a59c5e05c7fc6db2
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-19 17:13:02 +0900

    Feat. GameManager.cs 생성
    전반적인 게임을 관리

---
+++ Metaverse_Personal/Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // ΩÃ±€≈Ê
+    public static GameManager Instance;
+    
+}
