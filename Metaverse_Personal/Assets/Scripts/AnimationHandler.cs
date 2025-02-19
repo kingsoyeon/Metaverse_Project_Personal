71c5bc4ee1e7b1f898dbd89bff364a0e44cc791f
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-19 14:33:55 +0900

    Feat: AnimationController Move함수
    캐릭터가 움직이는지 판단(0.5f이상)하고 움직이는 애니메이션 출력

--- Metaverse_Personal/Assets/Scripts/AnimationHandler.cs
+++ Metaverse_Personal/Assets/Scripts/AnimationHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AnimationHandler : MonoBehaviour
 {
+    // 파라미터를 해시값으로 변경
     private static readonly int IsMoving = Animator.StringToHash("IsMove");
 
     protected Animator animator;
@@ -13,4 +14,9 @@ public class AnimationHandler : MonoBehaviour
         animator = GetComponentInChildren<Animator>();
     }
 
+    protected virtual void Move(Vector2 obj)
+    {
+        // 벡터의 길이가 0.5보다 크면 움직이는 애니메이션 출력
+        animator.SetBool(IsMoving, obj.magnitude>.5f);
+    }
 }
