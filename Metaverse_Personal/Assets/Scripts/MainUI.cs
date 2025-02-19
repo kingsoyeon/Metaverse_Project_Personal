004d6cbfca8d67d6ce405ce5df2f22a31d19fab5
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-19 20:25:42 +0900

    Feat: Main/MiniGameUI.cs 상속
    baseUI를 상속받음

--- Metaverse_Personal/Assets/Scripts/MainUI.cs
+++ Metaverse_Personal/Assets/Scripts/MainUI.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainUI : MonoBehaviour
+public class MainUI : BaseUI
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    protected override UIState GetUIState()
     {
-        
+        throw new System.NotImplementedException(); 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Metaverse_Personal/Assets/Scripts/MiniGameUI.cs
+++ Metaverse_Personal/Assets/Scripts/MiniGameUI.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MiniGameUI : MonoBehaviour
+public class MiniGameUI : BaseUI
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override UIState GetUIState()
     {
-        
+        throw new System.NotImplementedException();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
