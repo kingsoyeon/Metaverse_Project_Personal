2f72ae09cdbd756b0fec0032e584eec7584fd760
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-19 14:26:30 +0900

    Feat: AnimationHandler.cs 생성
    AnimationHandler로 애니메이션을 전체적으로 관리.
    자식클래스에서 애니메이터를 가져옴

---
+++ Metaverse_Personal/Assets/Scripts/AnimationHandler.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHandler : MonoBehaviour
+{
+    private static readonly int IsMoving = Animator.StringToHash("IsMove");
+
+    protected Animator animator;
+
+    protected virtual void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+}
