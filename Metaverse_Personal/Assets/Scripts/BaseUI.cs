673bebc99e8a2ed282342afb7ce540831a796924
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-19 20:19:22 +0900

    Feat: GetUIState, SetActive
    UI state를 가져오고, ui 실행 여부를 결정하는 메서드

--- Metaverse_Personal/Assets/Scripts/BaseUI.cs
+++ Metaverse_Personal/Assets/Scripts/BaseUI.cs
@@ -6,9 +6,19 @@ public abstract class BaseUI : MonoBehaviour
 {
     protected UIManager uiManager;
 
+    // uimanager 참조
     public virtual void Init(UIManager uiManager)
     {
         this.uiManager = uiManager;
+    }
+
+    // 자식 클래스에서 UI state 가져옴
+    protected abstract UIState GetUIState();
+
+    // 현재 UI가 state와 같으면 gameObject.SetActive(true); 실행 (UI를 켠다.)
+    public void SetActive(UIState state)
+    {
+        gameObject.SetActive(GetUIState() == state);
     }
 
 }
