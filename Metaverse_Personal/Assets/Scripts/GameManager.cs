69e043925044a86150cf54035d9830361ac1d449
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-20 15:30:27 +0900

    Feat: AddScore 메서드 추가
    게임매니저에서 addscore관장

--- Metaverse_Personal/Assets/Scripts/GameManager.cs
+++ Metaverse_Personal/Assets/Scripts/GameManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    
     public static GameManager Instance;
+    public UIManager uiManager;
     
     // 싱글톤
     private void Awake()
@@ -14,4 +16,25 @@ public class GameManager : MonoBehaviour
         else Destroy(gameObject);
     }
 
+    private int cureentScore = 0;
+
+    // 게임 오버
+    public void GameOver()
+    {
+
+        
+    }
+
+    // 게임 재시작
+    public void GameRestart()
+    {
+        
+
+    }
+
+    // 점수 추가
+    public void AddScore(int score)
+    {
+        cureentScore += score;
+    }
 }
