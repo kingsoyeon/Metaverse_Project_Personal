ff83bdccd3a71184198d98a4a35978c8249f67b9
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-20 12:31:10 +0900

    Feat: 카메라가 플레이어에서 떨어져서 따라감
    offset 간격만큼 x좌표가 떨어져서 플레이어를 따라감

--- Metaverse_Personal/Assets/Scripts/FollowCamera.cs
+++ Metaverse_Personal/Assets/Scripts/FollowCamera.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class FollowCamera : MonoBehaviour
 {
+    
+    public Transform miniGameCat; // 미니게임 플레이어
+    float offsetX;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (miniGameCat == null) return;
+
+        offsetX = transform.position.x- miniGameCat.position.x; // 간격은 카메라 위치 - 플레이어 위치만큼 (음수)
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if(miniGameCat == null) return;
+
+        Vector3 pos = transform.position; // pos에 카메라 위치 저장
+        pos.x = miniGameCat.position.x + offsetX; // 음수를 더하므로 왼쪽에서 따라옴
+        transform.position = pos; // 바뀐 x좌표 다시 저장
     }
 }
