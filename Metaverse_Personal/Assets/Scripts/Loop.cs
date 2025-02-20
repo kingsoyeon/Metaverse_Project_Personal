f79ee2563d23e5c5908b93352d1942f6b91be1ee
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-20 14:47:08 +0900

    Fix: 배경이 사라지는것처럼 보이는 issue해결
    background 사이즈가 크기 때문에, Looper도 보다 멀리 떨어뜨려 배경이 자연스럽게 이어지도록함

--- Metaverse_Personal/Assets/Scripts/Loop.cs
+++ Metaverse_Personal/Assets/Scripts/Loop.cs
@@ -7,6 +7,9 @@ public class Loop : MonoBehaviour
     public int pipeCount = 0;
     public Vector3 pipeLastPosition = Vector3.zero;
 
+    // 백그라운드 개수
+    public int BgCount = 1;
+
     void Start()
     {
         Obstacles[] obstacles = GameObject.FindObjectsOfType<Obstacles>();
@@ -14,16 +17,31 @@ public class Loop : MonoBehaviour
         pipeLastPosition = obstacles[0].transform.position;
         pipeCount = obstacles.Length;
 
+
+
         // 파이프 반복 랜덤 생성
         for (int i=0; i<pipeCount; i++)
         {
             pipeLastPosition = obstacles[i].RandomPipe(pipeLastPosition, pipeCount);
+
         }
     }
 
    
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // 배경 반복생성
+        if (collision.CompareTag("Background"))
+        {
+            float widthOfBgObject = ((BoxCollider2D)collision).size.x; //box collder size만큼 생성
+            Vector3 pos = collision.transform.position;
+
+            pos.x += widthOfBgObject * BgCount;
+            collision.transform.position = pos;
+            return;
+        }
+        
+        
         // 충돌했을때 파이프라면
         Obstacles pipe = collision.GetComponent<Obstacles>();
         if (pipe)
