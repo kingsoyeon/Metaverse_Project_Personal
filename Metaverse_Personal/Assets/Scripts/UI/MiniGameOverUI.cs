fc3c277dee30a4522b61ab685cdf0cf03ff469c7
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-20 17:18:29 +0900

    Feat: 게임오버UI 버튼연결
    RESTRAT->게임화면UI
    EXIT->씬로드(집)

--- Metaverse_Personal/Assets/Scripts/UI/MiniGameOverUI.cs
+++ Metaverse_Personal/Assets/Scripts/UI/MiniGameOverUI.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MiniGameOverUI : BaseUI
 {
+    [SerializeField] private Button restartBtn;
+    [SerializeField] private Button exitBtn;
+
     protected override UIState GetUIState()
     {
-        throw new System.NotImplementedException();
+        return UIState.MiniGameOver;
     }
 
     public override void Init(UIManager uiManager)
     {
         base.Init(uiManager);
+
+        restartBtn = transform.Find("RestartBtn").GetComponent<Button>();
+        exitBtn = transform.Find("ExitBtn").GetComponent<Button>();
+
+        restartBtn.onClick.AddListener(OnClickReStartButton);
+        exitBtn.onClick.AddListener(OnClickExitButton);
     }
 
 
@@ -19,10 +29,16 @@ public class MiniGameOverUI : BaseUI
 
     // 미니게임 나가기 버튼
 
-    public void MiniEndButton() { }
+    public void OnClickExitButton() 
+    {
+        uiManager.MiniGameExit();
+    }
 
     // 미니게임 재시작 버튼
-    public void MiniRestartButton() { }
+    public void OnClickReStartButton() 
+    {
+        uiManager.MiniGameStart();
+    }
 }
     
 
