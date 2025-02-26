efeaeb9da5b6f63e862da40e5b7bf7c5cebbf3a1
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-26 10:47:42 +0900

    Fix: NPC 대사 팝업
    유령에 충돌시 NPC 대사팝업

--- Metaverse_Personal/Assets/Scripts/UI/DialogueUI.cs
+++ Metaverse_Personal/Assets/Scripts/UI/DialogueUI.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DialogueUI : MonoBehaviour
+
+
 {
-    // Start is called before the first frame update
+    public GameObject dialogueUI;
+
+    
     void Start()
     {
-        
+        dialogueUI.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("Player"))
+        
+        {
+            if (!Input.GetKeyDown(KeyCode.Escape))
+            {
+                dialogueUI.SetActive(true);
+            }
+            else { dialogueUI.SetActive(false); }
+        }
     }
 }
